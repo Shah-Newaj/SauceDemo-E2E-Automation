0919614e2ea121a546ff50589b27cf462c54510c
Shah Newaj <newajuiucse@gmail.com>
2025-11-18 21:10:08 +0600

    Sauce Demo Site E2E Automation

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -1,3 +1,5 @@
+from playwright.sync_api import expect
+
 from pages.login_page import LoginPage
 from pages.products_page import ProductsPage
 from pages.cart_page import CartPage
@@ -12,15 +14,28 @@ def test_saucedemo(page):
     # Login Page
     login.load()
     login.login("standard_user", "secret_sauce")
+    # Assert login success (URL should contain /inventory)
+    expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+    print("✅ URL matched successfully!")
 
     # Products Page
     products.add_backpack_to_cart()
     products.open_cart()
+    # Assert cart page title
+    expect(page).to_have_title("Swag Labs")
+    print("✅ Cart Page Title matched successfully!")
 
     # Cart Page
     cart.proceed_to_checkout()
+    # Assert we are on checkout step one page
+    expect(page).to_have_url("https://www.saucedemo.com/checkout-step-one.html")
+    print("✅ checkout step one page matched successfully!")
 
     # Checkout Page
     checkout.fill_checkout_info("Shah", "Newaj", "6000")
     checkout.continue_checkout()
     checkout.finish_order()
+    # Assert success message appears
+    success_msg = page.locator(".complete-header")
+    expect(success_msg).to_have_text("Thank you for your order!")
+    print("✅ Success message is shown after finishing the order")
